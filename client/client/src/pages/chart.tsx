3ed3c4c74b357dffca87939667d58ec45eaf1805
Sudhan007 <sudhancool87@gmail.com>
2024-10-08 13:15:38 +0530

    ssds

--- client/client/src/pages/chart.tsx
+++ client/client/src/pages/chart.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 import { Pie, PieChart } from "recharts";
-import { ChartConfig, ChartContainer, ChartTooltip } from "../components/ui/chart";
+import { ChartContainer, ChartTooltip } from "../components/ui/chart";
 
 export const description = "A pie chart with gradient background";
 
 const chartData = [
   { key: "Gold", values: 15, fill: "url(#gold-gradient)" },
   { key: "Crypto", values: 2, fill: "url(#crypto-gradient)" },
-  { key: "International Equity", values: 13, fill: "url(#international-gradient)" },
+  {
+    key: "International Equity",
+    values: 13,
+    fill: "url(#international-gradient)",
+  },
   { key: "Domestic Equity", values: 35, fill: "url(#domestic-gradient)" },
   { key: "Debt", values: 5, fill: "url(#debt-gradient)" },
   { key: "Realestate", values: 30, fill: "url(#realestate-gradient)" },
@@ -47,7 +51,8 @@ export default function Chart() {
   useEffect(() => {
     const handleScroll = () => {
       if (chartRef.current) {
-        const rect = chartRef.current.getBoundingClientRect();
+        // @ts-ignore
+        const rect: any = chartRef.current.getBoundingClientRect();
         const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
         setIsVisible(isVisible);
       }
@@ -59,118 +64,157 @@ export default function Chart() {
     };
   }, []);
 
-  return (<>
-    <div className="flex justify-around items-center">
-      <div>
-        <ChartContainer
-          config={chartConfig}
-          className="aspect-square max-h-[600px]" // Increased size for container
-        >
-          <div ref={chartRef}>
-            <PieChart width={600} height={600}>
-              <svg>
-                <defs>
-                  <linearGradient id="gold-gradient" x1="0" y1="0" x2="1" y2="1">
-                    <stop offset="24.4%" stopColor="#18C5F3" />
-                    <stop offset="51.91%" stopColor="#49DBDA" />
-                    <stop offset="89.04%" stopColor="#94F9B3" />
-                  </linearGradient>
+  return (
+    <>
+      <div className='flex justify-around items-center'>
+        <div>
+          <ChartContainer
+            config={chartConfig}
+            className='aspect-square max-h-[600px]' // Increased size for container
+          >
+            <div ref={chartRef}>
+              <PieChart width={600} height={600}>
+                <svg>
+                  <defs>
+                    <linearGradient
+                      id='gold-gradient'
+                      x1='0'
+                      y1='0'
+                      x2='1'
+                      y2='1'>
+                      <stop offset='24.4%' stopColor='#18C5F3' />
+                      <stop offset='51.91%' stopColor='#49DBDA' />
+                      <stop offset='89.04%' stopColor='#94F9B3' />
+                    </linearGradient>
 
-                  <linearGradient id="debt-gradient" x1="0" y1="0" x2="1" y2="1">
-                    <stop offset="-44.8%" stopColor="#2C6470" />
-                    <stop offset="99.27%" stopColor="#54BFD6" />
-                  </linearGradient>
+                    <linearGradient
+                      id='debt-gradient'
+                      x1='0'
+                      y1='0'
+                      x2='1'
+                      y2='1'>
+                      <stop offset='-44.8%' stopColor='#2C6470' />
+                      <stop offset='99.27%' stopColor='#54BFD6' />
+                    </linearGradient>
 
-                  <radialGradient id="crypto-gradient" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-                    <stop offset="0%" stopColor="#FDFF69" />
-                    <stop offset="100%" stopColor="#F7CB46" />
-                  </radialGradient>
+                    <radialGradient
+                      id='crypto-gradient'
+                      cx='50%'
+                      cy='50%'
+                      r='50%'
+                      fx='50%'
+                      fy='50%'>
+                      <stop offset='0%' stopColor='#FDFF69' />
+                      <stop offset='100%' stopColor='#F7CB46' />
+                    </radialGradient>
 
-                  <linearGradient id="international-gradient" x1="0" y1="0" x2="1" y2="1">
-                    <stop offset="0%" stopColor="#FF4F4F" />
-                    <stop offset="50%" stopColor="#D45E44" />
-                    <stop offset="100%" stopColor="#D42727" />
-                  </linearGradient>
+                    <linearGradient
+                      id='international-gradient'
+                      x1='0'
+                      y1='0'
+                      x2='1'
+                      y2='1'>
+                      <stop offset='0%' stopColor='#FF4F4F' />
+                      <stop offset='50%' stopColor='#D45E44' />
+                      <stop offset='100%' stopColor='#D42727' />
+                    </linearGradient>
 
-                  <linearGradient id="domestic-gradient" x1="0" y1="0" x2="1" y2="1">
-                    <stop offset="12.1%" stopColor="#FF6444" />
-                    <stop offset="56.23%" stopColor="#FFA12B" />
-                    <stop offset="85.55%" stopColor="#F6C61E" />
-                  </linearGradient>
+                    <linearGradient
+                      id='domestic-gradient'
+                      x1='0'
+                      y1='0'
+                      x2='1'
+                      y2='1'>
+                      <stop offset='12.1%' stopColor='#FF6444' />
+                      <stop offset='56.23%' stopColor='#FFA12B' />
+                      <stop offset='85.55%' stopColor='#F6C61E' />
+                    </linearGradient>
 
-                  <linearGradient id="realestate-gradient" x1="0" y1="0" x2="1" y2="1">
-                    <stop offset="22.14%" stopColor="#6C26E8" />
-                    <stop offset="48.72%" stopColor="#7D5BF9" />
-                    <stop offset="75.24%" stopColor="#9193E9" />
-                  </linearGradient>
-                </defs>
-              </svg>
+                    <linearGradient
+                      id='realestate-gradient'
+                      x1='0'
+                      y1='0'
+                      x2='1'
+                      y2='1'>
+                      <stop offset='22.14%' stopColor='#6C26E8' />
+                      <stop offset='48.72%' stopColor='#7D5BF9' />
+                      <stop offset='75.24%' stopColor='#9193E9' />
+                    </linearGradient>
+                  </defs>
+                </svg>
 
-              <ChartTooltip
-                content={({ payload }) => {
-                  if (payload && payload.length > 0) {
-                    const { key, values } = payload[0].payload;
+                <ChartTooltip
+                  content={({ payload }) => {
+                    if (payload && payload.length > 0) {
+                      const { key, values } = payload[0].payload;
+                      return (
+                        <div className='bg-white px-2 py-1 rounded'>
+                          <p>{`${key}: ${values}%`}</p>
+                        </div>
+                      );
+                    }
+                    return null;
+                  }}
+                />
+                <Pie
+                  data={chartData}
+                  dataKey='values'
+                  nameKey='key'
+                  outerRadius={180}
+                  labelLine={false}
+                  label={({
+                    cx,
+                    cy,
+                    midAngle,
+                    innerRadius,
+                    outerRadius,
+                    value,
+                  }) => {
+                    const RADIAN = Math.PI / 180;
+                    const radius =
+                      innerRadius + (outerRadius - innerRadius) * 0.8;
+                    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
                     return (
-                      <div className="bg-white px-2 py-1 rounded">
-                        <p>{`${key}: ${values}%`}</p>
-                      </div>
+                      <text
+                        x={x}
+                        y={y}
+                        fill='black'
+                        textAnchor={"middle"}
+                        dominantBaseline='central'
+                        fontSize={11}>
+                        {`${value}%`}
+                      </text>
                     );
-                  }
-                  return null;
-                }}
-              />
-              <Pie
-                data={chartData}
-                dataKey="values"
-                nameKey="key"
-                outerRadius={180}
-                labelLine={false}
-                label={({ cx, cy, midAngle, innerRadius, outerRadius, value, name }) => {
-                  const RADIAN = Math.PI / 180;
-                  const radius = innerRadius + (outerRadius - innerRadius) * 0.80;
-                  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-                  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-                  return (
-                    <text
-                      x={x}
-                      y={y}
-                      fill="black"
-                      textAnchor={"middle"}
-                      dominantBaseline="central"
-                      fontSize={11}
-                    >
-                      {`${value}%`}
-                    </text>
-                  );
-                }}
-                isAnimationActive={isVisible}
-              />
-            </PieChart>
-          </div>
-        </ChartContainer>
-      </div>
-
-      <div>
-        <div className="flex flex-col gap-[30px]">
-          {chartData.map((entry) => (
-            <div key={entry.key} className="flex items-center gap-[20px] font-light ">
-           
-              <svg width={32} height={32} radius={180}>
-              <circle cx="16" cy="16" r="16" fill={entry.fill} />
-              </svg>
-              <span className="text-[25px]" >{entry.key}</span> 
+                  }}
+                  isAnimationActive={isVisible}
+                />
+              </PieChart>
             </div>
-          ))}
+          </ChartContainer>
+        </div>
+
+        <div>
+          <div className='flex flex-col gap-[30px]'>
+            {chartData.map((entry) => (
+              <div
+                key={entry.key}
+                className='flex items-center gap-[20px] font-light '>
+                <svg width={32} height={32} radius={180}>
+                  <circle cx='16' cy='16' r='16' fill={entry.fill} />
+                </svg>
+                <span className='text-[25px]'>{entry.key}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-      
-    </div>
-    <div className="text-center mt-[40px]">
-    <h1 className="text-[40px] font-normal text-[#6B6B6B]">Here’s the diverse portfolio</h1>
-  </div>
-  </>
-  
-    
+      <div className='text-center mt-[40px]'>
+        <h1 className='text-[40px] font-normal text-[#6B6B6B]'>
+          Here’s the diverse portfolio
+        </h1>
+      </div>
+    </>
   );
 }
--- client/client/src/pages/yourGoal.tsx
+++ client/client/src/pages/yourGoal.tsx
@@ -1,25 +1,21 @@
-import One from "../assets/01.png"
-import {
-    ChartConfig,
-    ChartContainer,
-    ChartTooltip,
-  } from "../components/ui/chart"
-  import { Bar, BarChart, CartesianGrid, XAxis, YAxis, Text } from "recharts"
+import One from "../assets/01.png";
+import { ChartConfig, ChartContainer } from "../components/ui/chart";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
 
 const chartData = [
   { name: "Tab 1", value: 90 },
   { name: "Tab 2", value: 30 },
   { name: "Tab 3", value: 50 },
-]
+];
 
 const chartConfig = {
   value: {
     label: "Value",
     color: "#60a5fa",
   },
-} satisfies ChartConfig
+} satisfies ChartConfig;
 
-const renderCustomBarShape = (props) => {
+const renderCustomBarShape = (props: any) => {
   const { x, y, width, height, index } = props;
 
   const fillColor = (() => {
@@ -29,71 +25,109 @@ const renderCustomBarShape = (props) => {
     return "#60a5fa";
   })();
 
-  return <rect x={x} y={y} width={width} height={height} fill={fillColor} radius={0} />;
+  return (
+    <rect
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      fill={fillColor}
+      radius={0}
+    />
+  );
 };
 
-
 export default function YourGoal() {
-    
-    return (
-        <div className="mt-[124px]">
-            <div className="flex flex-col justify-center items-center">
-                <div className="flex flex-col items-center justify-center gap-[31px]">
-                    <img src={One} className="w-[158px] h-[128px]" alt="01" />
-                    <p className="text-[42px] bg-[linear-gradient(91.73deg,#1B2EA1_48.22%,#090F34_127.67%)] bg-clip-text text-transparent text-[42px] font-bold"> Identify your Goal</p>
-                </div>
-                <div className="text-[35px] w-3/4 mt-[51px] text-center font-light text-[#535252]">
-                    List of your goals with details of how much you need to achieve the goal, how much you have invested for that goal, and how much you will need at the current inflation rate
-                </div>
+  return (
+    <div className='mt-[124px]'>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col items-center justify-center gap-[31px]'>
+          <img src={One} className='w-[158px] h-[128px]' alt='01' />
+          <p className='text-[42px] bg-[linear-gradient(91.73deg,#1B2EA1_48.22%,#090F34_127.67%)] bg-clip-text text-transparent text-[42px] font-bold'>
+            {" "}
+            Identify your Goal
+          </p>
+        </div>
+        <div className='text-[35px] w-3/4 mt-[51px] text-center font-light text-[#535252]'>
+          List of your goals with details of how much you need to achieve the
+          goal, how much you have invested for that goal, and how much you will
+          need at the current inflation rate
+        </div>
+      </div>
+
+      <div className='flex mt-[74px] mb-[56px] justify-center items-center'>
+        <div className='w-5/6'>
+          <div className='w-[400px] h-[312px] rounded bg-white shadow-lg  border border-gray-100  p-5'>
+            <div>
+              <p className='text-[18px] font-medium'>Emergency Fund (1Y)</p>
             </div>
-
-<div className="flex mt-[74px] mb-[56px] justify-center items-center">
-<div className="w-5/6">
-<div className="w-[400px] h-[312px] rounded bg-white shadow-lg  border border-gray-100  p-5">
-    <div>
-        <p className="text-[18px] font-medium">Emergency Fund (1Y)</p>
-    </div>
-    <div>
-        <div className=" mt-[60px]">
-        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-                                    <BarChart data={chartData} width={400} height={300}>
-                                        <defs>
-                                            <linearGradient id="gradient1" x1="0" y1="0" x2="1" y2="0">
-                                                <stop offset="29.17%" stopColor="#2552C5" />
-                                                <stop offset="70.83%" stopColor="#1E5FFF" />
-                                            </linearGradient>
-                                            <linearGradient id="gradient2" x1="0" y1="0" x2="1" y2="0">
-                                                <stop offset="29.17%" stopColor="#288528" />
-                                                <stop offset="70.83%" stopColor="#23B923" />
-                                            </linearGradient>
-                                            <linearGradient id="gradient3" x1="0" y1="0" x2="1" y2="0">
-                                                <stop offset="29.17%" stopColor="#C52525" />
-                                                <stop offset="70.83%" stopColor="#EB5F5F" />
-                                            </linearGradient>
-                                        </defs>
-                                        <XAxis dataKey="name" tickLine={false} axisLine={{ stroke: '#000' }} /> {/* Show X-axis */}
-                                        <YAxis domain={[0, 100]} hide />  {/* Hide Y-axis */}
-
-                                        {/* Use Bar with customized rendering */}
-                                        <Bar
-                                            dataKey="value"
-                                            radius={4}
-                                            barSize={40}
-                                            shape={renderCustomBarShape}  // Custom bar rendering
-                                        />
-
-                                        {/* Custom horizontal baseline (X-axis line) */}
-                                        <line x1="0" y1={290} x2="400" y2={290} stroke="#000" strokeWidth="2" /> {/* Baseline */}
-
-                                        {/* <ChartTooltip /> */}
-                                    </BarChart>
-                                </ChartContainer>
+            <div>
+              <div className=' mt-[60px]'>
+                <ChartContainer
+                  config={chartConfig}
+                  className='min-h-[200px] w-full'>
+                  <BarChart data={chartData} width={400} height={300}>
+                    <defs>
+                      <linearGradient
+                        id='gradient1'
+                        x1='0'
+                        y1='0'
+                        x2='1'
+                        y2='0'>
+                        <stop offset='29.17%' stopColor='#2552C5' />
+                        <stop offset='70.83%' stopColor='#1E5FFF' />
+                      </linearGradient>
+                      <linearGradient
+                        id='gradient2'
+                        x1='0'
+                        y1='0'
+                        x2='1'
+                        y2='0'>
+                        <stop offset='29.17%' stopColor='#288528' />
+                        <stop offset='70.83%' stopColor='#23B923' />
+                      </linearGradient>
+                      <linearGradient
+                        id='gradient3'
+                        x1='0'
+                        y1='0'
+                        x2='1'
+                        y2='0'>
+                        <stop offset='29.17%' stopColor='#C52525' />
+                        <stop offset='70.83%' stopColor='#EB5F5F' />
+                      </linearGradient>
+                    </defs>
+                    <XAxis
+                      dataKey='name'
+                      tickLine={false}
+                      axisLine={{ stroke: "#000" }}
+                    />{" "}
+                    {/* Show X-axis */}
+                    <YAxis domain={[0, 100]} hide /> {/* Hide Y-axis */}
+                    {/* Use Bar with customized rendering */}
+                    <Bar
+                      dataKey='value'
+                      radius={4}
+                      barSize={40}
+                      shape={renderCustomBarShape} // Custom bar rendering
+                    />
+                    {/* Custom horizontal baseline (X-axis line) */}
+                    <line
+                      x1='0'
+                      y1={290}
+                      x2='400'
+                      y2={290}
+                      stroke='#000'
+                      strokeWidth='2'
+                    />{" "}
+                    {/* Baseline */}
+                    {/* <ChartTooltip /> */}
+                  </BarChart>
+                </ChartContainer>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-</div>
-</div>
-
-        </div>
-    )
+  );
 }
